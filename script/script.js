aa72985262350b5e6af2feec25be7c25745d3047
shehab99b <shehabulislam429@gmail.com>
2025-03-01 17:28:25 +0600

    added the plus minus part of this page

--- script/script.js
+++ script/script.js
@@ -6,3 +6,42 @@ document.getElementById('color-btn').addEventListener('click', function(){
     currentColorIndex = (currentColorIndex + 1) % colors.length;
 })
 
+function goToAnotherPage() {
+    window.location.href = 'index.html'; 
+}
+function goToTheOtherPage() {
+    window.location.href = 'another-page.html'; 
+}
+
+const date = new Date();
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    document.getElementById("current-day").innerText = days[date.getDay()];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    document.getElementById("current-date").innerText = `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+
+
+
+
+const alertBtn = document.getElementsByClassName('task-btn');
+const taskCount = document.getElementById('minus-count')
+const taskPlusCount = document.getElementById('plus-count');
+
+for(let i = 0; i < alertBtn.length; i++){
+    alertBtn[i].addEventListener('click', function(){
+        alertBtn[i].classList.add('bg-gray-400', 'text-gray-500', 'cursor-not-allowed');
+        alertBtn[i].disabled = true;
+        alert("Board Updated Successfully");
+
+        let currentCount = parseInt(taskCount.innerText)
+        if(currentCount > 0){
+            taskCount.innerText = currentCount -1;
+        }
+
+        let currentPlusCount = parseInt(taskPlusCount.innerText);
+        if (currentPlusCount > 0) {
+            taskPlusCount.innerText = currentPlusCount +1;
+        }
+    })
+}
+
+
